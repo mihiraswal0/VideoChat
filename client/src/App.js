bb30a95704ecbee21685e83c03fb5ba29b113136
Mihir Aswal <montyaswal0@gmail.com>
2023-09-03 21:27:28 +0530

    added login & signup page

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
+import "./App.css";
 import React from 'react'
+
 import {BrowserRouter ,Route} from 'react-router-dom';
 import Homepage from './pages/Homepage';
 import Chatpage from './pages/Chatpage';
 const App = () => {
   return (
-    <div className='App'>
+    <div className="App">
     <BrowserRouter>
       <Route path='/' component={Homepage} exact/>
       <Route path='/chats' component={Chatpage}/>
