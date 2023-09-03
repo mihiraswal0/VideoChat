42e575c2cfc6e4dbf3c0e1eb68ba9641ab460280
Mihir Aswal <montyaswal0@gmail.com>
2023-09-03 14:21:57 +0530

    react app created

---
+++ server/data/data.js
@@ -0,0 +1,102 @@
+const chats = [
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "John Doe",
+          email: "john@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+      ],
+      chatName: "John Doe",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Guest User",
+          email: "guest@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+      ],
+      chatName: "Guest User",
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Anthony",
+          email: "anthony@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+      ],
+      chatName: "Anthony",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "John Doe",
+          email: "jon@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+        {
+          name: "Guest User",
+          email: "guest@example.com",
+        },
+      ],
+      chatName: "Friends",
+      groupAdmin: {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    },
+    {
+      isGroupChat: false,
+      users: [
+        {
+          name: "Jane Doe",
+          email: "jane@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+      ],
+      chatName: "Jane Doe",
+    },
+    {
+      isGroupChat: true,
+      users: [
+        {
+          name: "John Doe",
+          email: "jon@example.com",
+        },
+        {
+          name: "Piyush",
+          email: "piyush@example.com",
+        },
+        {
+          name: "Guest User",
+          email: "guest@example.com",
+        },
+      ],
+      chatName: "Chill Zone",
+      groupAdmin: {
+        name: "Guest User",
+        email: "guest@example.com",
+      },
+    },
+  ];
