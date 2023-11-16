590cc6630cb7248642baa0281106b462f081bd57
Mihir Aswal <montyaswal0@gmail.com>
2023-11-16 23:18:52 +0530

    deply

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.use('/api/message',messageRoutes);
 
   app.get("/", (req, res) => {
     res.send("API is running..");
- 
+  })
 
 // --------------------------deployment------------------------------
 
